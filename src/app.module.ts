dc15538fdf6d69c9772d802549853c6acdacd378
Andrey <ahrocha@gmail.com>
2026-02-19 09:34:03 -0300

    feat: adiciona logger personalizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,53 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
+import { CustomLogger } from './common/log/custom.logger';
+
+const isTest = process.env.NODE_ENV === 'test';
+const isDevelopment = process.env.NODE_ENV !== 'production' && !isTest;
 
 @Module({
-  imports: [],
+  imports: [
+    PinoLoggerModule.forRoot({
+      pinoHttp: {
+        level: 'trace',
+        serializers: {
+          req: (req) => ({
+            id: req.id,
+            method: req.method,
+            url: req.url,
+            query: req.query,
+            params: req.params,
+          }),
+          res: (res) => ({
+            statusCode: res.statusCode,
+          }),
+        },
+        customLogLevel: (req, res, err) => {
+          if (res.statusCode >= 500) {
+            return 'error';
+          } else if (res.statusCode >= 400) {
+            return 'warn';
+          }
+          return 'info';
+        },
+        ...(isDevelopment && {
+          transport: {
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+              singleLine: false,
+              translateTime: 'SYS:standard',
+              ignore: 'pid,hostname',
+            },
+          },
+        }),
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CustomLogger],
+  exports: [CustomLogger],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomLogger } from './common/log/custom.logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'log', 'verbose'],
+  });
+
+  app.useLogger(app.get(CustomLogger));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
